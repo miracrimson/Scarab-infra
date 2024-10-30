600e7ee1e10897bc1f1ba34676f464d541d53a99
Xinran <miracrimson27@gmail.com>
2024-10-29 21:58:54 -0700

    Add .gitignore file

--- cse220/plot/plot_figures.sh
+++ cse220/plot/plot_figures.sh
@@ -2,10 +2,11 @@
 set -x #echo on
 cd "$(dirname "$0")"
 
-OUTPUT_DIR=/home/$USER/plot/$EXPERIMENT
+OUTPUT_DIR=/home/$USER/plot/lab2
 SIM_PATH=/home/$USER/exp/simulations
 DESCRIPTOR_PATH=/home/$USER/$EXPERIMENT.json
 
 mkdir -p $OUTPUT_DIR
 
-python3 plot_ipc.py -o $OUTPUT_DIR -d $DESCRIPTOR_PATH -s $SIM_PATH
+python3 lab2_part4.py -o $OUTPUT_DIR -d $DESCRIPTOR_PATH -s $SIM_PATH
+#python3 delete.py -d $DESCRIPTOR_PATH -s $SIM_PATH
